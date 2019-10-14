a3a7e9aeb716ddf66107f73440382d4206d25f6d
rafgonsi <33317987+rafgonsi@users.noreply.github.com>
2019-10-14 20:40:36 +0200

    Add installation script
    This bash script should be run on ec2 instance dedicated to data downloading. The script installs anaconda, creates environment, installs required packages and sets cron job.

---
+++ installation_download_data_ec2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e  # fail in case of any error
+
+sudo timedatectl set-timezone Europe/Warsaw
+
+wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh -O Anaconda_installer.sh
+chmod +x Anaconda_installer.sh
+./Anaconda_installer.sh -b -p ~/Anaconda3
+rm Anaconda_installer.sh
+
+eval "$(/home/ubuntu/Anaconda3/bin/conda shell.bash hook)"
+
+conda create -n air-download-data python=3.6.9 -y
+conda activate air-download-data
+conda install -c anaconda pandas=0.25.1 requests=2.22.0 retrying=1.3.3 -y
+conda install -c conda-forge boto3=1.9.243 -y
+
+echo '@reboot /home/ubuntu/air-pollution/startup.sh' | sudo crontab -
+touch home/ubuntu/air-pollution/last_run.log
