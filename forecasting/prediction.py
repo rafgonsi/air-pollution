19ae9fa887327b4ba5f35d8405241ae1ba038300
rafgonsi <rafgonsi@gmail.com>
2019-10-20 20:00:49 +0200

    Remove unneeded variables

--- forecasting/prediction.py
+++ forecasting/prediction.py
@@ -2,12 +2,7 @@ from preprocessing import make_dataset
 
 
 if __name__ == '__main__':
-    BUCKET = 'air-pollution-data-pl'
-    ARCHIVE_FOLDER = 'historical-raw-data'
-    PARQUET_DATA_FILENAME = 'data.parquet'
-    PREFIX_FRESH_DATA = 'data'
-
-    df = make_dataset(Bucket=BUCKET,
-                      parquet_data_filename=PARQUET_DATA_FILENAME,
-                      prefix_fresh_data=PREFIX_FRESH_DATA,
-                      archive_folder=ARCHIVE_FOLDER)
+    df = make_dataset(Bucket='air-pollution-data-pl',
+                      parquet_data_filename='data.parquet',
+                      prefix_fresh_data='data',
+                      archive_folder='historical-raw-data')
